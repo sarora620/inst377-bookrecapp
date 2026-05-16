0fac422806462c4b21295e809fa1ba6e551d0eed
sarora620 <sarora21@terpmail.umd.edu>
2026-05-16 17:46:09 -0400

    added initial framework for supabase db's

--- expressapp/app.js
+++ expressapp/app.js
@@ -1,17 +1,36 @@
 // http://localhost:3000/pagename
 const express = require('express');
+// add supabase client
+const supabaseClient = require('@supabase/supabase-js');
 const app = express();
 const port = 3000;
+// add middleware
+app.use(bodyParser.json());
 
-// add public directory to connect front + backend 
+// ----------------------- add supabase db's ----------------------- // 
+// USERS DB //
+const supabaseURL_1 = '';
+const supabaseKey_1 = ''; // will link to dotenv
+
+// initialize the db 
+const supabase1 = supabaseClient.createClient(supabaseURL_1, supabaseKey_1);
+
+// SAVEDBOOKS DB //
+const supabaseURL_2 = '';
+const supabaseKey_2 = ''; // will link to dotenv
+
+// initialize the db 
+const supabase2 = supabaseClient.createClient(supabaseURL_2, supabaseKey_2);
+
+// ----------------------- add public directory to connect front + backend ----------------------- //
 app.use(express.static(__dirname + '/public'));
 
-// core API stuff goes here // 
+// ----------------------- core API stuff goes here ----------------------- // 
 
 
 // this goes at the END // 
 
-// serve them HTML pages!!!! 
+// ----------------------- serve them HTML pages!!!! ----------------------- //
 app.get('/', (req, res) => {
     res.sendFile('public/home.html', {root: __dirname});
 });
