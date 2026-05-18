01b6ca6fb924c9cc74b9f8501d1ebbe40a596638
sarora620 <sarora21@terpmail.umd.edu>
2026-05-17 22:42:40 -0400

    trying to fix my swiper

--- public/about.js
+++ public/about.js
@@ -1,51 +1,47 @@
-function loadCarousel(imgurls){
+function loadCarousel(imgurls) {
+    const wrapper = document.querySelector('.swiper-wrapper');
 
-        const wrapper = document.querySelector('.swiper-wrapper');
+    wrapper.innerHTML = ""; // clear first
 
-        wrapper.innerHTML = imgurls.map(url => {
-            const slide = document.createElement('div');
-            slide.classList.add('swiper-slide');
+    imgurls.forEach(url => {
+        const slide = document.createElement('div');
+        slide.classList.add('swiper-slide');
 
-            const img = document.createElement('img');
-            img.src = url;
-            img.alt = 'Carousel Image';
-    
-            slide.appendChild(img);
-            wrapper.appendChild(slide);
-  });
+        const img = document.createElement('img');
+        img.src = url;
+        img.alt = 'Carousel Image';
 
+        slide.appendChild(img);
+        wrapper.appendChild(slide);
+    });
 
-        const swiper = new Swiper('.swiper', {
-            effect: 'flip',
-            fadeEffect: {
-              crossFade: true
-            },
+    const swiper = new Swiper('.swiper', {
+        effect: 'flip',
 
-            direction: 'horizontal',
-            loop: true, 
-            
-            pagination: {
-              el: '.swiper-pagination',
-              clickable: true,
-            },
-          
-            navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-            },
-          
-            autoplay: {
-              delay: 5000,
-            },
-          });
+        direction: 'horizontal',
+        loop: true, 
+        
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+      
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+      
+        autoplay: {
+            delay: 5000,
+        },
+    });
 }
 
 const myBooks = [
-    'bookImages/tsoa.png',
-    'bookImages/rr.png',
-    'bookImages/scythe.jpg'
-  ];
-  
-  // Call the function to build the carousel
-  loadSwiperImages(myBooks);
+    'tsoa.png',
+    'rr.png',
+    'scythe.png'
+];
+
+loadCarousel(myBooks);
   
